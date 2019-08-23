93d68e83a474cafe48fbd7111135eb4452620556
Umwali Clara <umwaliclara@gmail.com>
2019-08-23 14:48:43 +0200

    create quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import{Quote} from './Quote'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes-App';
+  quotes : Quote[] =[
+    {id:1,name:"Umwiza First quote"},
+  ]
 }
