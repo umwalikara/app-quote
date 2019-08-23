a3ce9abefc7f8c4102fd561bef9e1210b14876d5
Umwali Clara <umwaliclara@gmail.com>
2019-08-23 20:09:32 +0200

    adding button for adding quotes

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Output, EventEmitter } from '@angular/core';
 import { Quote } from '../Quote';
 @Component({
   selector: 'app-quote-form',
@@ -7,6 +7,11 @@ import { Quote } from '../Quote';
 })
 export class QuoteFormComponent implements OnInit {
   newQuote = new Quote(0,"","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  submitQuote(){
+this.addQuote.emit(this.newQuote);
+  }
   constructor() { }
 
   ngOnInit() {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,28 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import{Quote} from '../Quote';
+import { Quote } from '../Quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes : Quote[] =[
-  new Quote (1,"Umwiza First quote","Good and Good",new Date(2019,8,23)),
+  quotes: Quote[] = [
+    new Quote(1, "Umwiza First quote", "Good and Good", new Date(2019, 8, 23)),
   ];
-  toggleDetails(index){
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
+  }
+  toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  // completeQuote(isComplete, index){
-  //   if (isComplete) {
-  //     this.quotes.splice(index,1);
-  //   }
-  // }
-  deleteQuote(isDelete, index){
+  deleteQuote(isDelete, index) {
     if (isDelete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
 
-      if (toDelete){
-        this.quotes.splice(index,1)
+      if (toDelete) {
+        this.quotes.splice(index, 1)
       }
     }
   }
